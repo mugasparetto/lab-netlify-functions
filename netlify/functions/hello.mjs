d3fce0135376450af2f298dd8e260813a8977ce5
mugasparetto <mu.gasparetto@hotmail.com>
2024-04-05 10:49:59 +0100

    allow cors

--- netlify/functions/hello.mjs
+++ netlify/functions/hello.mjs
@@ -1,3 +1,11 @@
 export default async (req, context) => {
-  return new Response('Hello, world!');
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers': 'Content-Type',
+      'Access-Control-Allow-Methods': 'GET',
+    },
+    body: 'This was a preflight call!',
+  };
 };
