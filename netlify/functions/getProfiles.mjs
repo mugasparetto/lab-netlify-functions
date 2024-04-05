d05be7ad44c08c0ac12a159f59203ed0ae2b1772
mugasparetto <mu.gasparetto@hotmail.com>
2024-04-05 14:13:45 +0100

    testing cors

--- netlify/functions/getProfiles.mjs
+++ netlify/functions/getProfiles.mjs
@@ -6,34 +6,43 @@ export default async (req, context) => {
     process.env.SUPABASE_API
   );
 
-  try {
-    const { data, error } = await supabase.from('profiles').select(`
-    id,
-    name,
-    skills:profile_skills (
-      details,
-      skill:skills (id, name)
-    )
-  `);
+  // try {
+  //   const { data, error } = await supabase.from('profiles').select(`
+  //   id,
+  //   name,
+  //   skills:profile_skills (
+  //     details,
+  //     skill:skills (id, name)
+  //   )
+  // `);
 
-    if (error) throw error;
+  //   if (error) throw error;
 
-    return new Response(JSON.stringify(data), {
-      statusCode: 200,
-      headers: {
-        'Access-Control-Allow-Origin': '*', //TODO: UPDATE THIS
-        'Access-Control-Allow-Headers': 'Content-Type',
-        'Access-Control-Allow-Methods': 'GET',
-      },
-    });
-  } catch (error) {
-    return new Response(JSON.stringify(error), {
-      statusCode: 500,
-      headers: {
-        'Access-Control-Allow-Origin': '*', //TODO: UPDATE THIS
-        'Access-Control-Allow-Headers': 'Content-Type',
-        'Access-Control-Allow-Methods': 'GET',
-      },
-    });
-  }
+  //   return new Response(JSON.stringify(data), {
+  //     statusCode: 200,
+  //     headers: {
+  //       'Access-Control-Allow-Origin': '*', //TODO: UPDATE THIS
+  //       'Access-Control-Allow-Headers': 'Content-Type',
+  //       'Access-Control-Allow-Methods': 'GET',
+  //     },
+  //   });
+  // } catch (error) {
+  //   return new Response(JSON.stringify(error), {
+  //     statusCode: 500,
+  //     headers: {
+  //       'Access-Control-Allow-Origin': '*', //TODO: UPDATE THIS
+  //       'Access-Control-Allow-Headers': 'Content-Type',
+  //       'Access-Control-Allow-Methods': 'GET',
+  //     },
+  //   });
+  // }
+
+  return new Response(JSON.stringify('Hello'), {
+    status: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*', //TODO: UPDATE THIS
+      'Access-Control-Allow-Headers': 'Content-Type',
+      'Access-Control-Allow-Methods': 'GET',
+    },
+  });
 };
