364a544ef00c45ab1afa7abf7b9d77d4af1a9772
mugasparetto <mu.gasparetto@hotmail.com>
2024-04-05 14:28:53 +0100

    added query

--- netlify/functions/getProfiles.mjs
+++ netlify/functions/getProfiles.mjs
@@ -6,12 +6,32 @@ export default async (req, context) => {
     process.env.SUPABASE_API
   );
 
-  return new Response(JSON.stringify(['Hello', 'World']), {
-    statusCode: 200,
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Headers': 'Content-Type',
-      'Access-Control-Allow-Methods': 'GET',
-    },
-  });
+  const headers = {
+    'Access-Control-Allow-Origin': '*', //TODO: UPDATE THIS
+    'Access-Control-Allow-Headers': 'Content-Type',
+    'Access-Control-Allow-Methods': 'GET',
+  };
+
+  try {
+    const { data, error } = await supabase.from('profile').select(`
+    id,
+    name,
+    skills:profile_skills (
+      details,
+      skill:skills (id, name)
+    )
+  `);
+
+    if (error) throw error;
+
+    return new Response(JSON.stringify(data), {
+      statusCode: 200,
+      headers,
+    });
+  } catch (error) {
+    return new Response(JSON.stringify(error), {
+      statusCode: 500,
+      headers,
+    });
+  }
 };
