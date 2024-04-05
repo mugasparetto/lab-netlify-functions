00a91133d952efda27620c47a573b0068bf78e48
mugasparetto <mu.gasparetto@hotmail.com>
2024-04-05 11:04:44 +0100

    response as json

--- netlify/functions/hello.mjs
+++ netlify/functions/hello.mjs
@@ -1,10 +1,13 @@
 export default async (req, context) => {
-  return new Response(['Hello', 'World'], {
-    statusCode: 200,
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Headers': 'Content-Type',
-      'Access-Control-Allow-Methods': 'GET',
-    },
-  });
+  return new Response(
+    { data: ['Hello', 'World'] },
+    {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Content-Type',
+        'Access-Control-Allow-Methods': 'GET',
+      },
+    }
+  );
 };
