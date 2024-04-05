02eb1ac6fb3ee40eaedeecea07a4cc485c78e422
mugasparetto <mu.gasparetto@hotmail.com>
2024-04-05 14:25:45 +0100

    added supabase

--- netlify/functions/getProfiles.mjs
+++ netlify/functions/getProfiles.mjs
@@ -1,11 +1,10 @@
 import { createClient } from '@supabase/supabase-js';
 
 export default async (req, context) => {
-  console.log(process.env.SUPABASE_API);
-  // const supabase = createClient(
-  //   'https://ulelpxqzwblrqicwqhwz.supabase.co',
-  //   process.env.SUPABASE_API
-  // );
+  const supabase = createClient(
+    'https://ulelpxqzwblrqicwqhwz.supabase.co',
+    process.env.SUPABASE_API
+  );
 
   return new Response(JSON.stringify(['Hello', 'World']), {
     statusCode: 200,
