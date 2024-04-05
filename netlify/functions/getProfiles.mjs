76f5e281f0fe47d6a72e40f57c4ae768f9391e84
mugasparetto <mu.gasparetto@hotmail.com>
2024-04-05 14:00:26 +0100

    get from supabase

---
+++ netlify/functions/getProfiles.mjs
@@ -0,0 +1,34 @@
+import { createClient } from '@supabase/supabase-js';
+
+export default async (req, context) => {
+  const supabase = createClient(
+    'https://ulelpxqzwblrqicwqhwz.supabase.co',
+    process.env.SUPABASE_API
+  );
+
+  try {
+    const { data, error } = await supabase.from('profile').select(`
+    id,
+    name,
+    skills:profile_skills (
+      details,
+      skill:skills (id, name)
+    )
+  `);
+
+    if (error) throw error;
+
+    return new Response(JSON.stringify(data), {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Content-Type',
+        'Access-Control-Allow-Methods': 'GET',
+      },
+    });
+  } catch (error) {
+    return new Response(JSON.stringify(error), {
+      statusCode: 500,
+    });
+  }
+};
